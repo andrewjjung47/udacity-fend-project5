cd6b15cf9571de60dbb7a01ff2744f2d61cce9c5
Andrew Jongho Jung <andrewjjung47@gmail.com>
2015-06-23 11:26:10 -0400

    Prototype layout and funtionality.

--- app/googlemaps.js
+++ app/googlemaps.js
@@ -1,11 +1,40 @@
-var map, markers, infoWindow;
+var GOOGLEMAPS = (function() {
+  var map, markers;
+  var infowindow = new google.maps.InfoWindow();
 
-function initialize() {
-  var mapOptions = {
-    center: { lat: 34.0204989, lng: -118.4117325},
-    zoom: 8
+  function initialize() {
+    var mapOptions = {
+      center: { lat: 34.0204989, lng: -118.4117325}, // Los Angeles as initial position
+      zoom: 8
+    };
+
+    map = new google.maps.Map(document.getElementById('map-canvas'),
+        mapOptions);
+  }
+  google.maps.event.addDomListener(window, 'load', initialize);
+
+  function createMarker(title, position) {
+    var marker = new google.maps.Marker({
+      position: position,
+      title: title
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+        infowindow.setContent(this.title);
+        infowindow.open(map, this);
+    });
+
+    marker.setMap(map);
+
+    return marker;
+  }
+
+  return {
+    createMarker: createMarker
   };
-  var map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
-}
-google.maps.event.addDomListener(window, 'load', initialize);
+})();
+
+
+
+
+
--- app/main.js
+++ app/main.js
@@ -2,8 +2,27 @@
 // define(['component/componentName/file'])
 
 define(["jquery", "knockout"], function($, ko) {
-  var viewModel = {
-    status: ko.observable('active')
-  };
-  ko.applyBindings(viewModel, $('html')[0]);
+  function Place(name, position) {
+    // TODO: automatically search position
+    return GOOGLEMAPS.createMarker(name, position);
+  }
+
+  function viewModel() {
+    var self = this;
+
+    self.listOfPlaces = ko.observableArray();
+
+    self.addPlace = function(name, position) {
+      var place = new Place(name, position);
+      self.listOfPlaces.push(place);
+    };
+
+    self.addPlace("Universal Studios Hollywood",
+                  new google.maps.LatLng(34.138117,-118.353378));
+    self.addPlace("Disneyland Park",
+                  new google.maps.LatLng(33.812092,-117.918974));
+
+    //GOOGLEMAPS.createMarker(self.listOfPlaces()[0]);
+  }
+  ko.applyBindings(new viewModel(), $('html')[0]);
 });
