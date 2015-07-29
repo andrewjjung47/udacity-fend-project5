8b32509f928dd4b132bb76abb6acef9cc505a310
Andrew Jongho Jung <andrewjjung47@gmail.com>
2015-07-28 23:51:35 -0400

    Start using Require.js syntax sugar.

--- app/main.js
+++ app/main.js
@@ -1,49 +1,53 @@
 // to depend on a bower installed component:
 // define(['component/componentName/file'])
 
-define(["jquery", "knockout"], function($, ko) {
-    function Place(name, position) {
-        // TODO: automatically search position
-        return GOOGLEMAPS.createMarker(name, position);
-    }
+define(function (require) {
+  var $ = require("jquery");
+  var ko = require("knockout");
 
-    function viewModel() {
-        var self = this;
 
-        var placesArray = [];
+  function Place(name, position) {
+      // TODO: automatically search position
+      return GOOGLEMAPS.createMarker(name, position);
+  }
 
-        self.listOfPlaces = ko.observableArray();
+  function viewModel() {
+      var self = this;
 
-        self.addPlace = function(name, position) {
-            var place = new Place(name, position);
-            self.listOfPlaces.push(place);
-        };
+      var placesArray = [];
 
-        self.removePlace = function() {
-            this.setMap(null);
-            self.listOfPlaces.remove(this);
-        };
+      self.listOfPlaces = ko.observableArray();
 
-        self.addPlace("Universal Studios Hollywood",
-                      new google.maps.LatLng(34.138117,-118.353378));
-        self.addPlace("Disneyland Park",
-                      new google.maps.LatLng(33.812092,-117.918974));
-        self.addPlace("Hollywood Walk of Fame",
-                      new google.maps.LatLng(34.0937508,-118.3264781));
-        self.addPlace("Griffith Observatory",
-                      new google.maps.LatLng(34.1184341,-118.3003935));
+      self.addPlace = function(name, position) {
+          var place = new Place(name, position);
+          self.listOfPlaces.push(place);
+      };
 
-        self.toggleInfoWindow = function() {
-            google.maps.event.trigger(this, 'click');
-        };
+      self.removePlace = function() {
+          this.setMap(null);
+          self.listOfPlaces.remove(this);
+      };
 
-        var filterSearch = function(target, keyword) {
-            return target.search(new RegExp(keyword, 'i'));
-        };
+      self.addPlace("Universal Studios Hollywood",
+                    new google.maps.LatLng(34.138117,-118.353378));
+      self.addPlace("Disneyland Park",
+                    new google.maps.LatLng(33.812092,-117.918974));
+      self.addPlace("Hollywood Walk of Fame",
+                    new google.maps.LatLng(34.0937508,-118.3264781));
+      self.addPlace("Griffith Observatory",
+                    new google.maps.LatLng(34.1184341,-118.3003935));
 
-        self.filteredPlaces = ko.computed(function() {
+      self.toggleInfoWindow = function() {
+          google.maps.event.trigger(this, 'click');
+      };
 
-        });
-    }
-    ko.applyBindings(new viewModel(), $('html')[0]);
+      var filterSearch = function(target, keyword) {
+          return target.search(new RegExp(keyword, 'i'));
+      };
+
+      self.filteredPlaces = ko.computed(function() {
+
+      });
+  }
+  ko.applyBindings(new viewModel(), $('html')[0]);
 });
